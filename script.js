f02d716705fe8627883732c0698fe057281db02f
chuongtlgts01549-star <chuongtlgts01549@gmail.com>
2026-03-29 01:19:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,21 @@ const videos=[
 
 {title:"Epic Music",id:"dQw4w9WgXcQ"},
 {title:"Gaming Highlights",id:"L_jWHffIx5E"},
-{title:"Best EDM Mix",id:"fLexgOxsZu0"},
+{title:"EDM Mix",id:"fLexgOxsZu0"},
 {title:"Coding Tutorial",id:"3fumBcKC6RE"},
 {title:"LoFi Chill",id:"5qap5aO4i9A"},
-{title:"Relaxing Piano",id:"2OEL4P1Rz04"},
-{title:"Space Documentary",id:"GOAEIMx39-w"},
+{title:"Relax Piano",id:"2OEL4P1Rz04"},
 {title:"Nature Film",id:"aqz-KE-bpKQ"},
-{title:"Drone Footage",id:"hHW1oY26kxQ"},
-{title:"Motivation",id:"ZXsQAXx_ao0"}
+{title:"Drone Footage",id:"hHW1oY26kxQ"}
 
 ];
 
 
 function loadVideos(){
 
-let container=document.getElementById("videos");
+const container=document.getElementById("videos");
 
 if(!container) return;
-
-container.innerHTML="";
 
 videos.forEach(v=>{
 
@@ -44,4 +40,49 @@ container.innerHTML+=`
 
 }
 
+
+
+function loadWatch(){
+
+const params=new URLSearchParams(window.location.search);
+
+const id=params.get("id");
+
+const player=document.getElementById("player");
+
+if(player) player.src="https://www.youtube.com/embed/"+id;
+
+
+
+const suggestions=document.getElementById("suggestions");
+
+if(!suggestions) return;
+
+
+
+videos.forEach(v=>{
+
+suggestions.innerHTML+=`
+
+<div class="video">
+
+<a href="watch.html?id=${v.id}">
+
+<img src="https://img.youtube.com/vi/${v.id}/0.jpg">
+
+<p>${v.title}</p>
+
+</a>
+
+</div>
+
+`;
+
+});
+
+}
+
+
 loadVideos();
+
+loadWatch();
